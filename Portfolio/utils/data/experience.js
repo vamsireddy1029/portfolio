0a99cc40e4ea2b062c9f610c5af4c3b6da5d0721
vamsireddy1029 <127970673+vamsireddy1029@users.noreply.github.com>
2024-09-03 21:28:01 +0530

    experience.js

--- Portfolio/utils/data/experience.js
+++ Portfolio/utils/data/experience.js
@@ -1,20 +1,14 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Full Stack PHP Developer',
-    company: "JNTU Kakinada",
-    duration: "(May 2022 - May 2024)"
+    title: 'Data Science Intern',
+    company: "HDLC Technologies",
+    duration: "(April 2022 - June 2022)"
   },
   {
     id: 2,
-    title: "MEAN Stack Developer & DB Admin",
-    company: "CTRI Rajahmundry",
-    duration: "(August 2023 - November 2023)"
-  },
-  {
-    id: 3,
-    title: "Drone Subsystem Programmer",
-    company: "TronTech Labs",
-    duration: "(May 2023 - July 2023)"
+    title: "Web development Intern",
+    company: "Main Flow Technologies",
+    duration: "(April 2023 - July 2023)"
   }
 ]
