c779ddf977aeba3f31d1073da1443bd8903c9f1f
vamsireddy1029 <127970673+vamsireddy1029@users.noreply.github.com>
2024-09-03 23:03:03 +0530

    personal-data.js

--- Portfolio/utils/data/personal-data.js
+++ Portfolio/utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Vamsi Reddy Dwarampudi",
-  profile: '/profile.jpg',
+  profile: '"C:\Users\ADMIN\Downloads\Portfolio\Portfolio\public\profile.png"',
   description: "My name is Vamsi Reddy, and I am a professional and enthusiastic programmer in my daily life. I have a strong commitment to continuous learning and a proactive approach to self-improvement. Exploring new technologies and solving intricate problems are my passions. I possess expertise in various domains including web application development, mobile app development, and machine learning, with a particular emphasis on leveraging JavaScript and Python for versatile solutions. I am eagerly seeking new opportunities that allow me to apply my skills effectively and contribute to impactful projects.",
   email: 'vamsireddy1029@gmail.com',
   phone: '6302549989',
